c9fc89aca3696d92e41f9c6773414d522e229f8e
Sean B <butts.sean01@gmail.com>
2020-08-02 10:03:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,18 +1,23 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["1","2","3","4"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  element= array.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
+     array = ["I", "am", "really", "learning"]
+    element = "wow"
+  array.first("wow")
   
-end
+  
+  end
+
 
 def remove_element_from_end_of_array(array)
   
